a0b2c0875366b9233ef1a64ca56abe6ddde0477a
ricardomiguez <ricardo.miguez@icloud.com>
2020-11-03 14:53:17 +0000

    Create challenge 01-02 folder and add solution to Objects Construction and Printing challenge

---
+++ challenge-01-02/objects-construction-and-printing.js
@@ -0,0 +1,14 @@
+// Rocketseat: LaunchBase Bootcamp
+// Challenge 01-02: Objects Construction and Printing
+
+const company = {
+    name: "Rocketseat",
+    color: "Purple",
+    focus: "Programming",
+    address: {
+        street: "Guilherme Gembala Street",
+        number: "260"
+    }
+};
+
+console.log(`The ${company.name} company is located at ${company.address.street}, ${company.address.number}.`);
