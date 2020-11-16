21462b434adde82453adfec896a16d1d08494a4f
ricardomiguez <ricardo.miguez@icloud.com>
2020-11-16 14:27:20 +0000

    Create challenge 04-01 folder and add solution to Header challenge

---
+++ challenge-04-01/routes.js
@@ -0,0 +1,19 @@
+/* Rocketseat: LaunchBase Bootcamp
+   Challenge 04-01: Header */
+
+const express = require("express");
+const routes = express.Router();
+
+routes.get("/", function(req, res) {
+    return res.redirect("/teachers");
+})
+
+routes.get("/teachers", function(req, res) {
+    return res.render("teachers/index");
+})
+
+routes.get("/students", function(req, res) {
+    return res.send("students");
+})
+
+module.exports = routes;
